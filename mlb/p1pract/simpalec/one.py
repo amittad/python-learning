cc77f61ae8a0db6fa2349d46b9a9382402b76106
amittad <tadamit2004@gmail.com>
2026-07-05 18:20:46 +0000

    strat practising

---
+++ mlb/p1pract/simpalec/one.py
@@ -0,0 +1,9 @@
+#Take a number and print whether it’s positive, negative, or zero.
+num=int(input("enter a number="))
+if num<0:
+    print("negative")
+elif num ==0:
+    print("zero")    
+
+elif num>0:
+    print("positive")    
